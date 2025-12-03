c971e8b5f341db6a1614ba79801c00bc1c3edf71
iarley_santos <iarleyconceicaodossantos@gmail.com>
2025-12-03 14:03:34 -0300

    GPS plot code update

--- data_acquisition/plot_data.m
+++ data_acquisition/plot_data.m
@@ -1,17 +1,25 @@
 clear all; clc;
 
 % Load the .mat files
-% 1. Use 'readtable' and specify the delimiter ';'
 serial_data= readtable('serial_data.csv', 'Delimiter', ';');
 
-% 2. Save the variable 'data' (which is a table) to a .mat file
 save('serial_data.mat', 'serial_data');
 
 disp('Conversion completed!');
 disp('Your data has been saved as a MATLAB "table".');
 
+set(0, 'DefaultFigureWindowStyle', 'normal');
+%% --- Maps --- %%
+figure(1)
+geoplot(serial_data.Lat, serial_data.Lon, 'r-', 'LineWidth', 1.5)
+hold on
+geoscatter(serial_data.Lat(1), serial_data.Lon(1), 50, 'go', 'filled')
+geoscatter(serial_data.Lat(end), serial_data.Lon(end), 50, 'ro', 'filled')
+geobasemap('satellite')
+title('GPS route on map')
+
 %% --- X Y--- %%
-figure(1);
+figure(2);
 plot(serial_data.X, serial_data.Y, 'r');
 title('X and Y position');
 xlabel('X (m)');
@@ -19,7 +27,7 @@ ylabel('Y (m)');
 grid on;
 
 %% --- Lat --- %%
-figure(2);
+figure(3);
 plot(serial_data.Lat, 'g')
 title('Latitude');
 xlabel('T');
@@ -27,7 +35,7 @@ ylabel('Lat');
 grid on;
 
 %% --- Lon --- %%
-figure(3);
+figure(4);
 plot(serial_data.Lon, 'b');
 title('Longitude');
 xlabel('T');
