cba0aa4c33e4b7c9f1cb79455e648eb7f26469f4
Heverton Souza <hevertonsilvadesouza@gmail.com>
2025-12-10 18:52:59 -0300

    Morato package created

---
+++ ros2_ws/src/morato/morato/get_image.py
@@ -0,0 +1,36 @@
+import cv2
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import CompressedImage
+
+class GetImages(Node):
+    def __init__(self):
+        super().__init__("get_images")
+        self.pub = self.create_publisher(CompressedImage, "camera/image/compressed", 10)
+        self.cap = cv2.VideoCapture(0)
+        self.timer = self.create_timer(0.03, self.callback) # 0.03 = 33FPS
+
+    def callback(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            return
+
+        # Encodes image as JPG
+        success, jpeg = cv2.imencode('.jpg', frame)
+        if not success:
+            return
+
+        msg = CompressedImage()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.format = "jpeg"
+        msg.data = jpeg.tobytes()
+
+        self.pub.publish(msg)
+
+def main():
+    rclpy.init()
+    node = GetImages()
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
---
+++ ros2_ws/src/morato/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'morato'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='morato',
+    maintainer_email='gestaogoytaborgs@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "get_linear_velocity = morato.get_linear_velocity:main",
+            "get_images = morato.get_images:main"
+
+        ],
+    },
+)
